da11d37b095ad5577de33b6f5617588adc598566
Crowned-Sparrow <phanthiphuongnga1979@gmail.com>
2025-08-04 12:27:12 +0700

    Update and rename init.sql to init_bronze.sql

---
+++ bronze/init_bronze.sql
@@ -0,0 +1,16 @@
+-- (1) Đảm bảo bảng phù hợp với dữ liệu
+IF OBJECT_ID('bronze.retail','U') IS NOT NULL
+    DROP TABLE bronze.retail;
+GO
+
+CREATE TABLE bronze.retail (
+	invoice_no   VARCHAR(50),
+	stock_code   VARCHAR(50),
+	descript     VARCHAR(255),
+	quantity     INT,
+	invoice_date DATETIME,
+	unit_price   DECIMAL(10,2),
+	customer_id  DECIMAL(10,0),
+	country      VARCHAR(100)
+);
+GO
