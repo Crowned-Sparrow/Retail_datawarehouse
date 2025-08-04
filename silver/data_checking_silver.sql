3c0fdd0bba7c6755dd6fbf64f0df59e55fca4f14
Crowned-Sparrow <phanthiphuongnga1979@gmail.com>
2025-08-04 14:00:48 +0700

    Create data_checking_silver.sql

---
+++ silver/data_checking_silver.sql
@@ -0,0 +1,44 @@
+-- Check duplicae --> have duplicate
+SELECT
+    invoice_no,
+    stock_code,
+    descript,
+    quantity,
+    invoice_date,
+    unit_price,
+    customer_id,
+    country,
+	COUNT(*)
+FROM
+    bronze.retail
+GROUP BY
+    invoice_no,
+    stock_code,
+    descript,
+    quantity,
+    invoice_date,
+    unit_price,
+    customer_id,
+    country
+HAVING COUNT(*) !=1
+-- Check quantity 
+  --<= 0 -->there are < 0
+SELECT 
+	*
+FROM 
+	bronze.retail
+WHERE quantity IS NULL OR quantity <= 0
+-- Check unit_price 
+  -- <=0 -->there are =0 and <0
+SELECT 
+	*
+FROM 
+	bronze.retail
+WHERE unit_price IS NULL OR unit_price <= 0
+  -- Check description of 0 unit_price
+SELECT DISTINCT
+	descript
+FROM 
+	bronze.retail
+WHERE unit_price = 0
+  --
