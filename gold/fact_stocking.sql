c71a21b1f3638a20afb2098399ece3fa7a85deda
Crowned-Sparrow <phanthiphuongnga1979@gmail.com>
2025-08-05 21:19:46 +0700

    Update fact_stocking.sql

--- gold/fact_stocking.sql
+++ gold/fact_stocking.sql
@@ -1,15 +1,14 @@
-CREATE OR ALTER VIEW gold.fact_stocking AS 
-  SELECT
-  	invoice_no,
-  	invoice_date,
-  	pr.product_key,
-  	cu.customer_key AS customer_key,
-  	quantity,
-  	unit_price,
-  	quantity* unit_price AS cost,
-  	description
-  FROM silver.retail main
-  LEFT JOIN gold.dim_customers cu
-  ON main.customer_id = main.customer_id
-  LEFT JOIN gold.dim_products pr
-  ON pr.product_id = main.stock_code
+CREATE OR ALTER VIEW gold.fact_stocking AS
+SELECT
+	invoice_no,
+	invoice_date,
+	cu.customer_key AS customer_key,
+	pr.product_key	AS product_key,
+	quantity,
+	unit_price,
+	unit_price*quantity AS cost
+ FROM silver.retail main
+ LEFT JOIN gold.dim_customers cu
+ ON main.customer_id = cu.customer_id
+ LEFT JOIN gold.dim_products pr
+ ON main.stock_code = pr.product_id
