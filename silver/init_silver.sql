bb7a6e6f066d6bc40a911c5d5f3fd553bd457d56
Crowned-Sparrow <phanthiphuongnga1979@gmail.com>
2025-08-05 14:23:02 +0700

    Create init_silver.sql

---
+++ silver/init_silver.sql
@@ -0,0 +1,11 @@
+CREATE TABLE silver.retail (
+	invoice_no	VARCHAR(50),
+	stock_code	VARCHAR(50),
+	product_name	VARCHAR(100),
+	description	VARCHAR(50),
+	customer_id	VARCHAR(50),
+	quantity	INT,
+	unit_price	DECIMAL(10,2),
+	country		VARCHAR(50),
+	invoice_date	DATETIME
+)
