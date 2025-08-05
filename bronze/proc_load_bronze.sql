0e011dd33db119b18d720ff14362353240aec39d
Crowned-Sparrow <phanthiphuongnga1979@gmail.com>
2025-08-05 12:47:16 +0700

    Update proc_load_bronze.sql

--- bronze/proc_load_bronze.sql
+++ bronze/proc_load_bronze.sql
@@ -1,27 +1,74 @@
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+CREATE OR ALTER PROCEDURE bronze.load_bronze
+    @file_path NVARCHAR(4000) = 'D:\AAA\Project\Data_warehouse\OnlineRetail.csv'
+AS
 BEGIN
-	BEGIN TRY
-		PRINT '>> Truncating bronze.retail'
-		TRUNCATE TABLE bronze.retail;
-		SET DATEFORMAT mdy;
+    SET XACT_ABORT ON;
+    SET NOCOUNT ON;
 
-		PRINT '>> Inserting bronze.retail'
+    BEGIN TRY
+        PRINT '>> Truncating bronze.retail';
+        TRUNCATE TABLE bronze.retail;
 
-		BULK INSERT bronze.retail
-		FROM 'D:\AAA\Project\Data_warehouse\OnlineRetail.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			ROWTERMINATOR = '0x0a',  -- đã thử  '\n' '\r\n'
-			TABLOCK
-		)
-	END TRY
-	BEGIN CATCH
-		PRINT	'========================================'
-		PRINT	'ERROR OCCURED DURING LOADING BRONZE LAYER'
-		PRINT	'Error Message' + ERROR_MESSAGE();
-		PRINT	'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
-		PRINT	'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
-		PRINT	'========================================'
-	END CATCH
-END
+        SET DATEFORMAT mdy;
+
+        PRINT '>> Inserting into bronze.retail from: ' + @file_path;
+
+        DECLARE @bulk_sql NVARCHAR(MAX) = '
+            BULK INSERT bronze.retail
+            FROM ''' + @file_path + '''
+            WITH (
+                FIRSTROW = 2,
+                FIELDTERMINATOR = '','',
+                ROWTERMINATOR = ''0x0a'',
+                TABLOCK
+            );
+        ';
+
+        EXEC sp_executesql @bulk_sql;
+
+        PRINT '>> Truncating bronze.retail_cleaned';
+        TRUNCATE TABLE bronze.retail_cleaned;
+
+        PRINT '>> Inserting into bronze.retail_cleaned';
+
+        WITH CTE_Duplicates AS (
+            SELECT *,
+                   ROW_NUMBER() OVER (
+                       PARTITION BY invoice_no, stock_code, UPPER(REPLACE(REPLACE(TRIM(descript), CHAR(13), ''), CHAR(10), '')), quantity, invoice_date, unit_price, customer_id, country
+                       ORDER BY (SELECT NULL)
+                   ) AS rn
+            FROM bronze.retail
+        )
+        INSERT INTO bronze.retail_cleaned (
+            invoice_no,
+            stock_code,
+            customer_id,
+            quantity,
+            unit_price,
+            invoice_date,
+            country,
+            description
+        )
+        SELECT
+            invoice_no,
+            stock_code,
+            customer_id,
+            quantity,
+            unit_price,
+            invoice_date,
+            country,
+            REPLACE(REPLACE(TRIM(descript), CHAR(13), ''), CHAR(10), '') AS description
+        FROM CTE_Duplicates
+        WHERE rn = 1;
+
+        PRINT '>> Load completed successfully.';
+    END TRY
+    BEGIN CATCH
+        PRINT '========================================';
+        PRINT 'ERROR OCCURRED DURING LOADING BRONZE LAYER';
+        PRINT 'Error Message: ' + ERROR_MESSAGE();
+        PRINT 'Error Number : ' + CAST(ERROR_NUMBER() AS NVARCHAR);
+        PRINT 'Error State  : ' + CAST(ERROR_STATE() AS NVARCHAR);
+        PRINT '========================================';
+    END CATCH
+END;
